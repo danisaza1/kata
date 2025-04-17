b0200f8a2893e7cb737638efba6bb2e66150a533
danisaza1 <dani_isaza@htomail.com>
2025-04-17 17:42:02 +0200

    terminar

--- Station_meteo/script.js
+++ Station_meteo/script.js
@@ -4,6 +4,10 @@ const button =document.getElementById("ok")
 const coordonnesCity = document.getElementById("gps");
 const temperatureCity = document.getElementById("temperature");
 const detailsCity = document.getElementById("details");
+const humidityCity = document.getElementById("precipitation");
+const graphiqueDays = document.getElementById('weatherChart')
+
+
 
 
 button.addEventListener("click", () => {
@@ -32,6 +36,8 @@ function fetchCoordinates(data) {
         coordonnesCity.innerText = '-';
         detailsCity.innerHTML = `Vérifiez le nom de la ville`
         temperatureCity.innerHTML = '-';
+        humidityCity.innerHTML= '-';
+        graphiqueDays.style.display= "none";
       }
 }
 
@@ -43,14 +49,61 @@ async function fetchWeather(lat, lon){
  
     const temperature = data.current.temperature_2m;
     temperatureCity.innerHTML = `${temperature}°C`;
+    const humidity = data.current.relative_humidity_2m
+    humidityCity.innerHTML = `Taux d'humidité: ${humidity} %`; 
     detailsCity.innerHTML = "Température actuelle";
+    weatherLastDays(lat, lon)
+}
+
+
+async function weatherLastDays (lat, lon) {
+    const url =  `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,precipitation&past_days=3`;
+    const reponse = await fetch(url);
+    const data = await reponse.json();
+
+    const times = data.hourly.time;
+    const temperatures = data.hourly.temperature_2m;
+   
+    graphique(times, temperatures, "Température (°C)", "rgba(255, 99, 132, 0.6)");
+} 
+
+let graphiqueIndex = null;
+function graphique(labels, data, label, color) {
+
+const graphique = graphiqueDays.getContext('2d');
+graphique.display= "block";
+
+if (graphiqueIndex) {
+    graphiqueIndex.destroy();
+} 
+
+graphiqueIndex= new Chart(graphique, {
+    type: 'bar',
+    data: {
+        labels: labels, // tiempo por hora
+        datasets: [{
+            label: label,
+            data: data,
+            backgroundColor: color,
+            borderColor: color,
+            borderWidth: 1,
+            pointRadius: 0, // para líneas suaves
+            tension: 0.2 // suaviza la curva
+        }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 }
 
 
 
-
-
-
+  
 
 
 
